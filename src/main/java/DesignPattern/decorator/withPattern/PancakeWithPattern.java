a866b553391d1b365557c51423ff021a6b979042
SuccessZhang <zhangzch9@yonyou.com>
2019-05-31 17:08:32 +0800

    装饰者模式

--- src/main/java/DesignPattern/decorator/withPattern/PancakeWithPattern.java
+++ src/main/java/DesignPattern/decorator/withPattern/PancakeWithPattern.java
@@ -6,6 +6,10 @@ import DesignPattern.decorator.common.Sausage;
 /**
  * @author SuccessZhang
  * 为方便形成鲜明对比，这里PancakeWithPattern就不继承于Pancake
+ * 要点：使用对象组合(has-a)/聚合(contanis-a)，而不是继承关系达到软件复用的目的
+ * 满足合成复用原则，如果扩展不多，则大大增加了系统的复杂性；
+ * 继承叫做白箱复用，相当于把所有的实现细节暴露给子类；
+ * 组合/聚合称之为黑箱复用，对类以外的对象是无法获取到实现细节的。
  */
 public class PancakeWithPattern {
 
