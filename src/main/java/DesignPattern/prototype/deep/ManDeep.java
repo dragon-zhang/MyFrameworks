441e975003216bc50fc6f888ce2f9ebe8ee43a1a
SuccessZhang <zhangzch9@yonyou.com>
2019-06-02 14:59:41 +0800

    原型模式

---
+++ src/main/java/DesignPattern/prototype/deep/ManDeep.java
@@ -0,0 +1,29 @@
+package DesignPattern.prototype.deep;
+
+import DesignPattern.prototype.common.Man;
+
+import java.io.*;
+
+/**
+ * @author SuccessZhang
+ */
+public class ManDeep extends Man implements Cloneable, Serializable {
+
+    @Override
+    public Object clone() {
+        //如果克隆的是单例，直接return this;
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            return ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
