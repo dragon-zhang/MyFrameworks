332fb0aa98d2af447aa9d8f5d23448017c52e277
SuccessZhang <zhangzch9@yonyou.com>
2019-06-03 18:54:42 +0800

    代理模式

---
+++ src/main/java/DesignPattern/proxy/Matchmaker.java
@@ -0,0 +1,24 @@
+package DesignPattern.proxy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author SuccessZhang
+ */
+public class Matchmaker {
+    private Map<String, Person> persons = new HashMap<>();
+
+    public void addSeeker(Person person) {
+        persons.put(person.getName(), person);
+    }
+
+    public void makeMatch() {
+        for (String name : persons.keySet()) {
+            System.out.println("我帮" + name + "物色对象");
+            persons.get(name).findLove();
+            System.out.println("物色成功，双方同意交往");
+            System.out.println(name + "给我钱");
+        }
+    }
+}
---
+++ src/main/java/DesignPattern/proxy/Person.java
@@ -0,0 +1,10 @@
+package DesignPattern.proxy;
+
+/**
+ * @author SuccessZhang
+ */
+public interface Person {
+    void findLove();
+
+    String getName();
+}
