6166b057e469075ba691e3ac6736b75f7e8e795c
SuccessZhang <zhangzch9@yonyou.com>
2019-07-02 21:34:30 +0800

    数据结构与算法：快速排序、归并排序

--- src/main/java/DataStructureAndAlgorithm/sort/SortUtil.java
+++ src/main/java/DataStructureAndAlgorithm/sort/SortUtil.java
@@ -56,6 +56,7 @@ public class SortUtil {
 
     /**
      * 堆排序
+     * 注意：虽然数组下标从0开始，但是大/小顶堆的根节点却是从1开始编号
      * 最好情况：O(nlogn)
      * 最坏情况：O(nlogn)
      * 辅助空间：O(1)
@@ -102,7 +103,51 @@ public class SortUtil {
      * @param array 原始数组
      */
     public static void mergeSort(int[] array) {
+        if (array == null || array.length <= 0) {
+            return;
+        }
+        int width = 1;
+        while (width < array.length) {
+            mergePass(array, width);
+            width *= 2;
+        }
+    }
 
+    private static void mergePass(int[] array, int width) {
+        int start = 0;
+        while (start + 2 * width - 1 < array.length) {
+            int mid = start + width - 1;
+            int end = start + 2 * width - 1;
+            merge(array, start, mid, end);
+            start = start + 2 * width;
+        }
+        //剩余无法构成完整的两组也要进行处理
+        if (start + width - 1 < array.length) {
+            merge(array, start, start + width - 1, array.length - 1);
+        }
+    }
+
+    private static void merge(int[] array, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+        int[] temp = new int[end + 1 - start];
+        int index = 0;
+        while (i <= mid && j <= end) {
+            if (array[i] <= array[j]) {
+                temp[index++] = array[i++];
+            } else {
+                temp[index++] = array[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[index++] = array[i++];
+        }
+        while (j <= end) {
+            temp[index++] = array[j++];
+        }
+        if (end + 1 - start >= 0) {
+            System.arraycopy(temp, 0, array, start, end + 1 - start);
+        }
     }
 
     /**
@@ -115,6 +160,44 @@ public class SortUtil {
      * @param array 原始数组
      */
     public static void quickSort(int[] array) {
+        if (array.length > 1) {
+            quickSort(array, 0, array.length - 1);
+        }
+    }
 
+    private static void quickSort(int[] a, int low, int high) {
+        //递归算法的出口
+        if (low > high) {
+            return;
+        }
+        int i = low;
+        int j = high;
+        int temp;
+        int key = a[low];
+        while (i < j) {
+            //完成一趟排序
+            while (i < j && a[j] > key) {
+                //从右往左找到第一个小于key的数
+                j--;
+            }
+            while (i < j && a[i] <= key) {
+                //从左往右找到第一个大于key的数
+                i++;
+            }
+            if (i < j) {
+                //交换两个数位置
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        //调整key的位置
+        temp = a[i];
+        a[i] = a[low];
+        a[low] = temp;
+        //key左边的数
+        quickSort(a, low, i - 1);
+        //key右边的数
+        quickSort(a, i + 1, high);
     }
 }
--- src/main/java/DataStructureAndAlgorithm/sort/test/SortTest.java
+++ src/main/java/DataStructureAndAlgorithm/sort/test/SortTest.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class SortTest {
     public static void main(String[] args) {
-        int[] array = new int[]{5, 2, 6, 0, 3, 9, 1, 7, 4, 8};
+        int[] array = new int[]{5, 2, 6, 0, 3, 9, 1, 7, 4, 8, 15, 14, 13, 12, 11, 10};
         int[] arrayCopy1 = array.clone();
         int[] arrayCopy2 = array.clone();
         int[] arrayCopy3 = array.clone();
@@ -21,8 +21,8 @@ public class SortTest {
         SortUtil.heapSort(arrayCopy2);
         System.out.println("heapSort:" + Arrays.toString(arrayCopy2));
         SortUtil.mergeSort(arrayCopy3);
-        System.out.println("heapSort:" + Arrays.toString(arrayCopy3));
+        System.out.println("mergeSort:" + Arrays.toString(arrayCopy3));
         SortUtil.quickSort(arrayCopy4);
-        System.out.println("heapSort:" + Arrays.toString(arrayCopy4));
+        System.out.println("quickSort:" + Arrays.toString(arrayCopy4));
     }
 }
