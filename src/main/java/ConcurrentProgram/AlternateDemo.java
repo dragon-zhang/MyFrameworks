4d254a9e32a7776f08e3f34837c800a19f086e22
SuccessZhang <zhangzch9@yonyou.com>
2019-09-25 23:08:24 +0800

    交替打印demo

--- src/main/java/ConcurrentProgram/AlternateDemo.java
+++ src/main/java/ConcurrentProgram/AlternateDemo.java
@@ -1,31 +1,101 @@
 package ConcurrentProgram;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author SuccessZhang
  */
 public class AlternateDemo {
 
-    static class MyThread implements Runnable {
+    public static class PrintChar extends Thread {
 
-        private static int i = 1;
+        private char[] chars;
+        private Condition cCondition, nCondition;
+        private Lock lock;
 
-        @Override
-        public synchronized void run() {
-            for (; i <= 52; i++) {
-                System.out.print(i);
-                if (i % 2 == 0) {
-                    System.out.print((char) ('A' + (i - 1) / 2) + " ");
-                }
-            }
+        public PrintChar(char[] chars, Lock lock, Condition cCondition, Condition nCondition) {
+            this.chars = chars;
+            this.nCondition = nCondition;
+            this.cCondition = cCondition;
+            this.lock = lock;
         }
 
+        @Override
+        public void run() {
+            for (char c : chars) {
+                System.out.print(c);
+                lock.lock();
+                try {
+                    cCondition.signal();
+                    nCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+            try {
+                lock.lock();
+                nCondition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static class PrintNum extends Thread {
+
+        private int[] numbers;
+        private Condition cCondition, nCondition;
+        private Lock lock;
+
+        public PrintNum(int[] numbers, Lock lock, Condition cCondition, Condition nCondition) {
+            this.numbers = numbers;
+            this.nCondition = nCondition;
+            this.cCondition = cCondition;
+            this.lock = lock;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < numbers.length; i += 2) {
+                System.out.println(numbers[i] + "" + numbers[i + 1]);
+                try {
+                    lock.lock();
+                    nCondition.signal();
+                    cCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+            try {
+                lock.lock();
+                nCondition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        Runnable runnable = new MyThread();
-        Thread thread1 = new Thread(runnable);
-        Thread thread2 = new Thread(runnable);
-        thread1.start();
-        thread2.start();
+        Lock lock = new ReentrantLock();
+        Condition cCondition = lock.newCondition();
+        Condition nCondition = lock.newCondition();
+        char[] c = new char[26];
+        int[] num = new int[52];
+        for (int i = 0; i < 52; i++) {
+            num[i] = i + 1;
+        }
+        for (int i = 0; i < 26; i++) {
+            c[i] = (char) (i + 65);
+        }
+        PrintChar printChar = new PrintChar(c, lock, cCondition, nCondition);
+        printChar.start();
+        PrintNum printNum = new PrintNum(num, lock, cCondition, nCondition);
+        printNum.start();
     }
 }
