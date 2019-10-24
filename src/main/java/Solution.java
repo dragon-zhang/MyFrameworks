f14d2312fb9c8930beb98721e94030284f72a760
SuccessZhang <zhangzch9@yonyou.com>
2019-10-24 17:01:11 +0800

    请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，则该路径不能再进入该格子。 例如 a b c e s f c s a d e e 矩阵中包含一条字符串"bcced"的路径，但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,33 +1,53 @@
-import java.util.Arrays;
-
 /**
  * @author SuccessZhang
  */
 public class Solution {
 
-    /**
-     * @param n 总人数
-     * @param m 约瑟夫环编号为m的人退出，剩下的继续组成约瑟夫环
-     * @param k 最后幸存的人数
-     */
-    public int[] LastRemaining_Solution(int n, int m, int k) {
-        if (n <= 0 || m <= 0) {
-            return new int[]{-1};
-        }
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = i;
-        }
-        for (int i = 2; i <= n; i++) {
-            for (int j = 0; j < k; j++) {
-                result[j] = (result[j] + m) % i;
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        boolean[] flags = new boolean[matrix.length];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (hasPath(matrix, rows, cols, i, j, str, 0, flags)) {
+                    return true;
+                }
             }
         }
-        return result;
+        return false;
+    }
+
+    public boolean hasPath(char[] matrix,
+                           int rows, int cols,
+                           int row, int col,
+                           char[] str,
+                           int k, boolean[] flags) {
+        int index = row * cols + col;
+        //终止条件
+        if (row < 0 || col < 0 || row >= rows || col >= cols ||
+                str[k] != matrix[index] ||
+                flags[index]) {
+            return false;
+        }
+        if (k == str.length - 1) {
+            return true;
+        }
+        //标志该点走过
+        flags[index] = true;
+        if (hasPath(matrix, rows, cols, row - 1, col, str, k + 1, flags) ||
+                hasPath(matrix, rows, cols, row + 1, col, str, k + 1, flags) ||
+                hasPath(matrix, rows, cols, row, col - 1, str, k + 1, flags) ||
+                hasPath(matrix, rows, cols, row, col + 1, str, k + 1, flags)) {
+            return true;
+        }
+        //行不通，回溯
+        flags[index] = false;
+        return false;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.LastRemaining_Solution(5, 3, 0)));
+        System.out.println(solution.hasPath(
+                "ABCESFCSADEE".toCharArray(),
+                3, 4,
+                "ABCCED".toCharArray()));
     }
 }
