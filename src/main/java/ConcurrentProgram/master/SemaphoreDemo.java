312baf3f7e8006ab869c9027c81c94b2fe96d2df
SuccessZhang <zhangzch9@yonyou.com>
2019-11-18 16:07:08 +0800

    SemaphoreDemo

--- src/main/java/ConcurrentProgram/master/SemaphoreDemo.java
+++ src/main/java/ConcurrentProgram/master/SemaphoreDemo.java
@@ -10,11 +10,11 @@ import java.util.concurrent.Semaphore;
  * 而是在需要同步的地方调用acquire(int permits)指定需要同步的线程个数。
  * <p>
  * 以下demo是用Semaphore来实现类似CyclicBarrier的可重用功能，可以发现
- * Semaphore比CyclicBarrier更加灵活，但是复杂的功能需要自己实现。
+ * Semaphore比CyclicBarrier更加灵活，但是定制化的功能需要自己实现。
  */
 public class SemaphoreDemo {
 
-    private static volatile Semaphore semaphore = new Semaphore(0);
+    private static Semaphore semaphore = new Semaphore(0);
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(2);
