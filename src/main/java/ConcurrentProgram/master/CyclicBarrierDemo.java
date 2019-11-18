9c83b4cb5cba78c9a48e9425a18abf954d6ba139
SuccessZhang <zhangzch9@yonyou.com>
2019-11-18 15:27:16 +0800

    CyclicBarrierDemo

---
+++ src/main/java/ConcurrentProgram/master/CyclicBarrierDemo.java
@@ -0,0 +1,50 @@
+package ConcurrentProgram.master;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author SuccessZhang
+ * {@link java.util.concurrent.CountDownLatch}是一次性的，
+ * {@link java.util.concurrent.CyclicBarrier}具有可复用性。
+ */
+public class CyclicBarrierDemo {
+
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(2, () -> {
+            //当所有子线程全部到达屏障后，将会执行这段代码
+            System.out.println("all threads has finished this step");
+        });
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " finished step1");
+                barrier.await();
+                System.out.println(Thread.currentThread().getName() + " finished step2");
+                barrier.await();
+                System.out.println(Thread.currentThread().getName() + " finished step3");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.submit(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " finished step1");
+                barrier.await();
+                System.out.println(Thread.currentThread().getName() + " finished step2");
+                barrier.await();
+                System.out.println(Thread.currentThread().getName() + " finished step3");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        });
+        executor.shutdown();
+    }
+
+}
